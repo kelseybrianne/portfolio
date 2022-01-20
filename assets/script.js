940970c7b63f880b67f81b9e8d95926975f94b35
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-20 10:26:59 -0800

    add scrollToTop function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 let footerEl = document.getElementById("currentYear");
+const scrollBtn = $('#scrollBtn')
 
 // Mobile menu
 $(document).ready(function() {
@@ -18,3 +19,16 @@ var currentYear = moment().format("YYYY");
 let copyrightEl = document.createElement("p");
 copyrightEl.innerHTML = `&copy; Kelsey Mailau ${currentYear}`;
 footerEl.appendChild(copyrightEl);
+
+// Create scroll to top function when button is clicked
+const scrollToTop = () => {
+    console.log('clicked');
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
+
+scrollBtn.click(scrollToTop);
+
